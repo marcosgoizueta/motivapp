d1ecb93a454ddb1cd8f0784968b60f511bf5506f
marcosgoi <goizuemarcos02@gmail.com>
2026-02-13 19:11:15 -0300

    feat: setup GoRouter with Riverpod provider and initial home route

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,1 +1,21 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+final appRouterProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+    ],
+  );
+});
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Home')));
+  }
+}
